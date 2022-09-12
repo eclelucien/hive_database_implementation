0389c6b592d3c05e0f8b4cd44df3c1167f26e687
eclelucien <eclesiastelucien@gmail.com>
2022-09-12 17:12:00 -0300

    demo

---
+++ demo_module/android/app/src/main/kotlin/com/example/demo_module/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.demo_module
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ demo_module/lib/models/data_model.g.dart
@@ -0,0 +1,47 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'data_model.dart';
+
+// **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+class DataModelAdapter extends TypeAdapter<DataModel> {
+  @override
+  final int typeId = 0;
+
+  @override
+  DataModel read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return DataModel(
+      title: fields[0] as String,
+      description: fields[1] as String,
+      complete: fields[2] as bool,
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, DataModel obj) {
+    writer
+      ..writeByte(3)
+      ..writeByte(0)
+      ..write(obj.title)
+      ..writeByte(1)
+      ..write(obj.description)
+      ..writeByte(2)
+      ..write(obj.complete);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is DataModelAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
